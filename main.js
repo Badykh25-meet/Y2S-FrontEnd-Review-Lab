cd905ccf0d70b7079b2ed33654afb6769d462afa
Badykh25-meet <badykh0@gmail.com>
2024-07-16 23:39:05 +0300

    n

---
+++ main.js
@@ -0,0 +1,16 @@
+
+  <h2>Pick Your Favorite Color</h2>
+    <form>
+        <input type="radio" id="colorRed" name="color" value="red">
+        <label for="colorRed">Red</label><br>
+        <input type="radio" id="colorBlue" name="color" value="blue">
+        <label for="colorBlue">Blue</label><br>
+        <input type="radio" id="colorGreen" name="color" value="green">
+        <label for="colorGreen">Green</label>
+    </form>
+
+    <button onclick="changeBackgroundColor('#ff69b4')">Change BG Color to Pink</button>
+    <button onclick="changeBackgroundColor('#00ff00')">Change BG Color to Lime</button>
+    <button onclick="changeBackgroundColor('#8a2be2')">Change BG Color to BlueViolet</button>
+</body>
+</html>
